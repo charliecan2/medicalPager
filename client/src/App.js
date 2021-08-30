f30e42b9c0b4e5e3e3f27a1babe0d89df8ed6142
ccantu20 <carloscq920@gmail.com>
2021-08-30 11:40:32 -0500

    Initialized react-app and installed several dependancies

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Medical Pager</h1>
+    </div>
+  );
+}
+
+export default App;
