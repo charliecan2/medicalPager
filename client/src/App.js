f44db2b65199657c4ecaf148a549e5d26042a2ae
ccantu20 <carloscq920@gmail.com>
2021-09-06 12:04:47 -0500

    Created the sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
+    <div className='app__wrapper'>
       <Chat client={client} theme='team light'>
         <ChannelListContainer />
         <ChannelContainer /> 
